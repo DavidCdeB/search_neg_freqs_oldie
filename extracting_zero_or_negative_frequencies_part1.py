5d8feee68823387be3f47468dc98f2f159227b7d
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-07-26 17:52:15 +0100

    Add files via upload

---
+++ extracting_zero_or_negative_frequencies_part1.py
@@ -0,0 +1,40 @@
+# 
+# Extracting zero or negative freq's from a set of *T.out
+
+import re
+import os
+import glob
+from itertools import islice
+import numpy as np
+import subprocess
+import itertools
+import sys
+
+
+ALL_FREQ = []
+path='./'
+template = os.path.join(path, '*T.out')
+
+
+for fname in glob.glob(template):
+  print fname
+  f = open(fname, 'r')
+  real_part = False
+
+  for line in f:
+
+        if line.startswith('  DISPERSION K POINT NUMBER'):
+           print line
+
+        elif line.startswith(' REAL PART'):
+            real_part = True
+#           print line
+        elif line.startswith(' NORMAL MODES NORMALIZED TO CLASSICAL AMPLITUDES'):
+            real_part = True
+#            print line
+        elif line.startswith(' IMAGINARY PART'):
+            real_part = False
+        elif line.startswith(' FREQ(') and real_part:
+             FREQS = line.split()
+             FREQS_only_zero_or_negative = [x for x in FREQS if x.startswith('0.00') or x.startswith('-')]
+             print FREQS_only_zero_or_negative
