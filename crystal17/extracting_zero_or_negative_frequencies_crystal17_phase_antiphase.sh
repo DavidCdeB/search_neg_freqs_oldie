cd864db5c2121dfc5844d46a22862ee6ff0302de
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-10-27 16:19:37 +0100

    Add files via upload

---
+++ crystal17/extracting_zero_or_negative_frequencies_crystal17_phase_antiphase.sh
@@ -0,0 +1,20 @@
+#
+
+# Program to exctract zero or neg. freqs:
+
+python extracting_zero_or_negative_frequencies_part1_crystal17_phase_antiphase.py > zero_or_negatives.txt
+
+ex zero_or_negatives.txt <<-EOF
+  :g/\[\]/d    
+  :g/^$/d      
+  :%s/\.\//\r\.\//g
+  wq " Update changes and quit.
+EOF
+
+# In order, these commands are:
+
+# remove lines with "[]"
+# remove emtpy lines
+# search for "./"
+# Add an empty line before "./"
+# Update changes and quit.
