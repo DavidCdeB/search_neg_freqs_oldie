a353862892aedb5d248507d0c5704313711fa45f
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-11-24 10:59:23 +0000

    Add files via upload

--- extract_neg_freqs_v3.sh
+++ extract_neg_freqs_v3.sh
@@ -16,7 +16,7 @@ ex neg_freqs_grabbing_more_decimals.txt <<-EOF
   wq " Update changes and quit.
 EOF
 
-echo "" >> neg_freqs_grabbing_more_decimals.txt # Add a new line at the end of the file to allow <extract_neg_freqs_v3_counting.py> to work
+#echo "" >> neg_freqs_grabbing_more_decimals.txt # Add a new line at the end of the file to allow <extract_neg_freqs_v3_counting.py> to work # This is not necessary
 echo "You can now open the 'neg_freqs_grabbing_more_decimals.txt' file, with the summary for all the negative freqs"
 
 echo "Counting the number of negative freqs..."
