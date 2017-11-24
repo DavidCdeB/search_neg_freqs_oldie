ea9c69d3e0ff8c53cb6ebd6f53d22c9afd55c376
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-11-24 10:41:30 +0000

    Add files via upload

--- extract_neg_freqs_v3_counting.py
+++ extract_neg_freqs_v3_counting.py
@@ -2,28 +2,49 @@
 
 import re
 import os
-import glob
-from itertools import islice
 import numpy as np
-import subprocess
-import itertools
-import sys
+import pdb
 
-f = open('neg_freqs_grabbing_more_decimals.txt', 'r')
-print f
-for line in f:
-        if re.match(r"^.", line):
-         print line
-         All_aux = []
-         while True:
-               target = f.next()
-               target2 = target.translate(None, "[]'',")
 
-               aux = target2.split()
-               All_aux.append(aux)
- 
-               flat_list = [item for sublist in All_aux for item in sublist]
-               if not aux:
-                 break
-         print 'len(negatives) = ' , len(flat_list)
+# None of the imports are required
+# We only need a count
+negatives = 0
+
+# Previously I was not closing the file
+# This closes it automagically
+with open('neg_freqs_grabbing_more_decimals.txt', 'r') as f:
+# print f
+  for line in f:
+         # No need for a regular expression here
+         if line.startswith("./"):
+           if negatives:
+              print 'len(negatives) = ', negatives, '\n'
+              negatives = 0
+           print line 
+
+         # Used "else if" since both `startswith` can't be true
+         elif line.startswith("["):
+#          print 'line = ', line
+
+#          print 'type(line) = ', type(line) 
+           # `line` is a  <type 'str'>. We need to change if to list,
+           #  in order to count. This requires 2 steps:
+
+           # 1st: Remove "[]''" from `line`:
+           target2 = line.translate(None, "[]'',")
+#          print 'target2 = ', target2
+           
+           # 2nd: split that line target2
+           aux = target2.split()
+#          print 'aux = ', aux
+#          print 'type(aux) = ', type(aux)
+           # `aux` is now a  <type 'list'>.
+
+#          print 'len(aux) = ', len(aux)
+#          negatives = negatives + len(aux)
+           negatives += len(aux) # more compact
+
+if negatives:
+   print 'len(negatives) = ' , negatives
+
 
